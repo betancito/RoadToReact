f584d15aba7c34c2f0758c28b55811b5b97fd0b2
Betancito <jeronimobeta90@gmail.com>
2024-09-19 05:02:44 -0500

    Finished components training

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import { TwitterFollowCard } from "./TwitterFollowCard"
+export function App() {
+    return(
+        <>
+            <TwitterFollowCard userName="jeronimoszs" name="Jeronimo Betancur Duque" />
+            <TwitterFollowCard userName="Betancito" name="Jeronimo betancur Duque" />
+            <TwitterFollowCard userName="anthonymartinez" name="Anthony Martinez" />
+        </>
+    )
+}
